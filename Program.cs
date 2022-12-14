a253ef5059eb363be3445ca4054453b4c8407d9d
Victor Hugo <victorhugo_mr@live.com>
2022-12-14 18:27:09 -0300

    Criando o projeto

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ByteBank{
+
+    public class Program{
+
+		public static void Menu(int option){
+			//Menu principal.
+			Console.WriteLine();
+   	}
+
+		public static void MenuTransactions(){
+			//Menu de transações.
+			Console.WriteLine();
+   	}
+
+		public static void InsertUser(){
+			//Inserir novo usuário.
+   	}
+
+		public static void DeleteUser(){
+			//Deletar um usuário.
+   	}
+
+		public static void ListAccounts(){
+			//Listar todas as contas registradas.
+   	}
+
+		public static void AccountDetails(){
+			//Detalhes de um usuário.
+   	}
+
+		public static void Balance(){
+			//Total armazenado no banco.
+   	}
+
+		public static void Transactions(){
+			//Manipular conta.
+			MenuTransactions();
+   	}
+
+		public static void Exit(){
+			//Sair do programa.
+   	}
+
+    	public static void Main(){
+			int option = 0;
+
+			do{
+				Menu(option);
+				option = int.Parse(Console.ReadLine());
+			}while(option != 0);
+   	}
+	}
+}
