033a47a1d21ecfefb3fc7726c1790686e67ee5ae
Victor Hugo <victorhugo_mr@live.com>
2022-12-16 20:45:53 -0300

    Criando função 'InsertUser'

--- Program.cs
+++ Program.cs
@@ -2,51 +2,168 @@
 
     public class Program{
 
-		public static void Menu(int option){
+		public class Account{
+			public string? Name { get; set; }
+			public string? CPF { get; set; }
+			public double? Balance { get; set; }
+		}
+
+		public static int Menu(){
 			//Menu principal.
-			Console.WriteLine();
+			List<Account> listOfAccounts = new List<Account>();
+
+			Console.Clear();
+			Console.WriteLine("Escolha uma opção: ");
+			Console.WriteLine("1. Inserir novo usuário");
+			Console.WriteLine("2. Deletar um usuário");
+			Console.WriteLine("3. Listar contas registradas");
+			Console.WriteLine("4. Mostrar detalhes de um usuário");
+			Console.WriteLine("5. Mostrar saldo de um usuário");
+			Console.WriteLine("6. Realizar uma transação");
+			Console.WriteLine("0. Sair");
+
+			int option = int.Parse(Console.ReadLine());
+
+			switch(option){
+					case 1:
+						InsertUser(listOfAccounts);
+						break;
+
+					case 2:
+						DeleteUser();
+						break;
+
+					case 3:
+						ListAccounts(listOfAccounts);
+						break;
+
+					case 4:
+						AccountDetails();
+						break;
+
+					case 5:
+						ShowBalance();
+						break;
+
+					case 6:
+						Transactions();
+						break;
+
+					case 0:
+						Exit();
+						break;
+				}
+
+			return option;
    	}
 
-		public static void MenuTransactions(){
+		public static int MenuTransactions(){
 			//Menu de transações.
-			Console.WriteLine();
+			Console.Clear();
+			Console.WriteLine("Escolha uma opção: ");
+			Console.WriteLine("1. Depositar");
+			Console.WriteLine("2. Sacar");
+			Console.WriteLine("3. Transferir");
+			Console.WriteLine("4. Voltar");
+
+			int option = int.Parse(Console.ReadLine());
+
+			switch(option){
+					case 1:
+						Deposit();
+						break;
+
+					case 2:
+						Withdraw();
+						break;
+
+					case 3:
+						Transfer();
+						break;
+
+					default:
+						break;
+				}
+			
+			return option;
    	}
 
-		public static void InsertUser(){
+		public static void InsertUser(List<Account> listOfAccounts){
 			//Inserir novo usuário.
+			Account account = new Account();
+
+			Console.Clear();
+			Console.WriteLine("Para inserir um novo usuário, digite o nome do usuário: ");
+			account.Name = Console.ReadLine();
+
+			Console.Clear();
+			Console.WriteLine("Digite o CPF do usuário: ");
+			account.CPF = Console.ReadLine();
+			account.Balance = 0;
    	}
 
 		public static void DeleteUser(){
 			//Deletar um usuário.
+			Console.Clear();
    	}
 
-		public static void ListAccounts(){
+		public static void ListAccounts(List<Account> listOfAccounts){
 			//Listar todas as contas registradas.
+			Console.Clear();
+			foreach(Account account in listOfAccounts){
+				Console.WriteLine(account.Name);
+			}
+			Console.ReadLine();
    	}
 
 		public static void AccountDetails(){
 			//Detalhes de um usuário.
+			Console.Clear();
    	}
 
-		public static void Balance(){
+		public static void ShowBalance(){
 			//Total armazenado no banco.
+			Console.Clear();
    	}
 
 		public static void Transactions(){
 			//Manipular conta.
-			MenuTransactions();
+			int optionTransactions = 4;
+
+			Console.Clear();
+			
+			do{
+				optionTransactions = MenuTransactions();
+
+			}while(optionTransactions != 4);
+   	}
+
+		public static void Deposit(){
+			//Depositar valor na conta.
+			Console.Clear();
+   	}
+
+		public static void Withdraw(){
+			//Sacar valor da conta.
+			Console.Clear();
+   	}
+
+		public static void Transfer(){
+			//Transferir valor para outra conta.
+			Console.Clear();
    	}
 
 		public static void Exit(){
 			//Sair do programa.
+			Console.Clear();
    	}
 
     	public static void Main(){
 			int option = 0;
 
 			do{
-				Menu(option);
-				option = int.Parse(Console.ReadLine());
+				option = Menu();
+
 			}while(option != 0);
    	}
 	}
